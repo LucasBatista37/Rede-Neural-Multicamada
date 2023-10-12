6bc1c871fde2a39779ce8a44459ddab44c4b48f1
Lucas Batista <124750301+LucasBatista37@users.noreply.github.com>
2023-10-12 18:30:23 -0300

    Add files via upload

--- rede_multicamada.py
+++ rede_multicamada.py
@@ -3,6 +3,14 @@ import numpy as np
 def sigmoid(soma):
     return 1 / (1 + np.exp(-soma))
 
+def sigmoidDerivada(sig):
+    return sig * (1 - sig)
+
+a = sigmoid(0.5)
+b = sigmoidDerivada(a)
+
+print(b)
+
 entradas = np.array([[0,0], [0,1], [1,0], [1,1]])
 saidas = np.array([[0], [1], [1], [0]])
 pesos0 = np.array([[-0.424, -0.740, -0.961], [0.358, -0.577, -0.469]])
@@ -17,5 +25,16 @@ for j in range(epocas):
     somaSinapse1 = np.dot(camadaOculta, pesos1)
     camadaSaida = sigmoid(somaSinapse1)
 
+    erroCamadaSaida = saidas - camadaSaida
+    mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    
+    derivadaSaida = sigmoidDerivada(camadaSaida)
+    deltaSaida = erroCamadaSaida * derivadaSaida
+
+    pesos1Trasposta = pesos1.T
+    deltaSaidaXPeso = deltaSaida.dot(pesos1Trasposta)
+    deltaCamadaOculta = deltaSaidaXPeso * sigmoidDerivada(camadaOculta)
+
 print(camadaOculta)
 print(camadaSaida)
+print(deltaSaida)
